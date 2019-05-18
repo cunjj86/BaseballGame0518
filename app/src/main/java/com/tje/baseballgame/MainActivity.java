9cbc020febeff2bb92e5a01d8c20d2193920a6dd
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-05-18 15:57:50 +0900

    컴퓨터가 랜덤 3자리 문제 출제

--- app/src/main/java/com/tje/baseballgame/MainActivity.java
+++ app/src/main/java/com/tje/baseballgame/MainActivity.java
@@ -1,14 +1,20 @@
 package com.tje.baseballgame;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tje.baseballgame.databinding.ActivityMainBinding;
+
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
+
+    int[] computerExamArray = new int[3]; // 741 => 7, 4, 1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         bindViews();
         setupEvents();
         setValues();
@@ -21,11 +27,55 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        makeExam();
 
     }
+
+    void makeExam() {
+
+        while (true) {
+            int randomNumber = (int) (Math.random() * 899 + 100); // Ex. 747
+
+            int[] tempNumber = new int[3];
+
+            tempNumber[0] = randomNumber / 100;
+            tempNumber[1] = randomNumber / 10 % 10;
+            tempNumber[2] = randomNumber % 10;
+
+            boolean isDuplOk = true;
+            if (tempNumber[0] == tempNumber[1] || tempNumber[1] == tempNumber[2] || tempNumber[0] == tempNumber[2]) {
+                isDuplOk = false;
+            }
+
+            boolean isZeroOk = true;
+            if (tempNumber[0] == 0 || tempNumber[1] == 0 || tempNumber[2] == 0) {
+                isZeroOk = false;
+            }
+
+            if (isDuplOk && isZeroOk) {
+                computerExamArray[0] = tempNumber[0];
+                computerExamArray[1] = tempNumber[1];
+                computerExamArray[2] = tempNumber[2];
+
+                break;
+            }
+
+        }
+
+
+    }
+
 
     @Override
     public void bindViews() {
 
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
+
+
+
+
+
+
+
